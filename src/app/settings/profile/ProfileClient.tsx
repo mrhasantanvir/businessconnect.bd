c56f333bea96db635b7f72c2e95cff90973749ab
mrhasantanvir <smmhasantanvir@gmail.com>
2026-05-03 23:27:11 +1000

    fix: restore button and correct tag structure in ProfileClient.tsx

--- src/app/settings/profile/ProfileClient.tsx
+++ src/app/settings/profile/ProfileClient.tsx
@@ -315,6 +315,7 @@ export default function ProfileClient({ user }: { user: any }) {
                         </div>
                       )}
                   </div>
+              </div>
           </div>
       </div>
     </div>
